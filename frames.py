bb4592ad8904cfaf264d580cb557910c31211dbd
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-05-29 20:11:35 +0800

    Update

--- frames.py
+++ frames.py
@@ -1,9 +1,11 @@
 from PIL import Image, ImageSequence
 import numpy as np
+import string
 
 
-def rgba_to_hex(rgba):
-    return f"#{rgba[0]:02x}{rgba[1]:02x}{rgba[2]:02x}{rgba[3]:02x}"
+def rgb_to_hex(rgba):
+    # I have removed the hashtag for compatibility
+    return f"{rgba[0]:02x}{rgba[1]:02x}{rgba[2]:02x}"
 
 def load_frames(image: Image, mode='RGBA'):
     # I got this fromn stack overflow
@@ -15,14 +17,33 @@ def load_frames(image: Image, mode='RGBA'):
 with Image.open('small.gif') as im:
     frames = load_frames(im)
 
+
 frames = [[
-    rgba_to_hex(frame[i, j])
-    for i in range(24) # Height
-    for j in range(32) # Width
+    rgb_to_hex(frame[i, j])
+    for i in range(30) # Height
+    for j in range(40) # Width
 ] for frame in frames]
 
+colors = set()
+for frame in frames:
+    colors.update(frame)
+
+# Remember to remove the #
+encoder = {color: letter for color, letter in zip(colors, string.ascii_lowercase)}
+header = ";".join(colors)
+
+encoded = [
+    [encoder[pixel] for pixel in frame]
+    for frame in frames
+]
+
 with open("output.txt", "w") as f:
-    for pixel in [x[1:] for xs in frames for x in xs]: # skip the # because it isn't needed in scratch
-        f.write(f"{pixel}\n")
+    # Scratch treats delimters as a CSV file if they are on the first line
+    f.write("\n") 
+    f.write(f"{header}\n")
+    for frame in encoded:
+        f.write(f"{';'.join(frame)}\n")
+    #for pixel in [x[1:] for xs in frames for x in xs]: # skip the # because it isn't needed in scratch
+    #    f.write(f"{pixel}\n")
 
     # frames * width * height
