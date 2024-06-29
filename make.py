598d41ed3dab856e419693a61c2a913ae8dbefb6
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-06-28 22:48:34 -0600

    Reformat code

--- make.py
+++ make.py
@@ -10,40 +10,41 @@ def rgb_to_hex(rgba):
     # I have removed the hashtag for compatibility
     return f"{rgba[0]:02x}{rgba[1]:02x}{rgba[2]:02x}"
 
-def load_frames(image: Image, mode='RGBA'):
+def load_frames(image: Image, mode="RGBA"):
     # I got this fromn stack overflow
-    return np.array([
-        np.array(frame.convert(mode))
-        for frame in ImageSequence.Iterator(image)
-    ])
+    return np.array(
+        [np.array(frame.convert(mode)) for frame in ImageSequence.Iterator(image)]
+    )
 
 def rle_compress(data):
-    #result = []
-    #for frame in data:
+    # result = []
+    # for frame in data:
     current_char = data[0]
     total = 0
     new = ""
-    for char in data[1:]: # current_char is already data[0]
+    for char in data[1:]:  # current_char is already data[0]
         if char == current_char:
             total += 1
         else:
             # TODO: If total = 1, then add current char twice (doesn't save any characters by encoding)
             if total > 0:
-                new = new + current_char + str(total) # we have already current char is already added (part of total)
+                new = (
+                    new + current_char + str(total)
+                )  # we have already current char is already added (part of total)
             else:
                 new = new + current_char
             total = 0
             current_char = char
 
-def make(fpath, width, height, output_path):
-    with Image.open(fpath) as im:
-        frames = load_frames(im)
-
-    frames = [[
-        rgb_to_hex(frame[i, j])
-        for i in range(height) # Height
-        for j in range(width) # Width
-    ] for frame in frames]
+def mk_color_map(frames, height, width):
+    frames = [
+        [
+            rgb_to_hex(frame[i, j])
+            for i in range(height)  # Height
+            for j in range(width)  # Width
+        ]
+        for frame in frames
+    ]
 
     colors = set()
     for frame in frames:
@@ -55,19 +56,26 @@ def make(fpath, width, height, output_path):
 
     # Remember to remove the #
     colorLookup = {color: letter for color, letter in zip(colors, CHARS)}
-    colorEncoded = [
-        [colorLookup[pixel] for pixel in frame]
-        for frame in frames
-    ]
+    colorEncoded = [[colorLookup[pixel] for pixel in frame] for frame in frames]
+
+    return colors, colorEncoded
+
+
+def make(fpath, width, height, output_path):
+    with Image.open(fpath) as im:
+        frames = load_frames(im)
+
+    colors, compressed = mk_color_map(frames, width, height)
 
     with open(output_path, "w") as f:
         # Scratch treats semicolons and commas as CSV delimeters
         f.write(f"{width}x{height}\n")
         f.write(f"{CHARS}\n")
-        f.write(f"{'-'.join(colors)}\n") # colors
+        f.write(f"{'-'.join(colors)}\n")  # colors
         # 3 lines for headers
-        for frame in "".join([rle_compress(frame) for frame in colorEncoded]):
+        for frame in "".join([rle_compress(frame) for frame in compressed]):
             f.write(f"{''.join(frame)}\n")
+
 
 if __name__ == "__main__":
     FPATH = sys.argv[1]
