6e45d2cf28d061cd3a45343264938ac5e0735202
ninjamar <70116773+ninjamar@users.noreply.github.com>
2024-06-29 11:23:53 -0600

    Hopefully added delta encoding

--- make.py
+++ make.py
@@ -6,15 +6,60 @@ import sys
 
 CHARS = """abcdefghijklmnopqrstuvwxyz!"#$%&'()*"""
 
+
 def rgb_to_hex(rgba):
     # I have removed the hashtag for compatibility
     return f"{rgba[0]:02x}{rgba[1]:02x}{rgba[2]:02x}"
+
 
 def load_frames(image: Image, mode="RGBA"):
     # I got this fromn stack overflow
     return np.array(
         [np.array(frame.convert(mode)) for frame in ImageSequence.Iterator(image)]
     )
+
+
+def delta_like(data):
+    composed = []
+    fmt = lambda start, end: "[{0}-{1}]".format(start, end)
+    # Each item
+    i = 1
+    while i < len(data):
+        curr_frame = data[i]
+        prev_frame = data[i - 1]
+
+        range_end = 0
+        is_in_range = False
+        temp = []
+
+        for index, (curr_char, prev_char) in enumerate(zip(curr_frame, prev_frame)):
+            if curr_char == prev_char:
+                if not is_in_range:
+                    range_start = index
+                    is_in_range = True
+            else:
+                if is_in_range:
+                    range_end = index - 1
+                    is_in_range = False
+
+                    # Python indexed
+                    f = fmt(range_start, range_end)
+                    if (range_end - range_start) > len(f):
+                        temp.append(f)
+                    else:
+                        temp.append(curr_frame[range_start:range_end])
+                else:
+                    temp.append(curr_char)
+        # Only thing I can think of
+        if len(temp) == 0:
+            if len(curr_frame) != len(prev_frame):
+                raise Exception("shouldn't happen")
+            temp.append(fmt(0, len(prev_frame)))
+
+        composed.append("".join(temp))
+        i += 1
+    return composed
+
 
 def rle(data):
     # Row length encoding
@@ -35,6 +80,7 @@ def rle(data):
             total = 0
             current_char = char
     return new
+
 
 def mk_color_map(frames, width, height):
     frames = [
@@ -60,16 +106,19 @@ def mk_color_map(frames, width, height):
 
     return colors, colorEncoded
 
+
 def compress(frames, width, height):
     colors, frames = mk_color_map(frames, width, height)
     frames = [rle(frame) for frame in frames]
+    frames = delta_like(frames)
     return colors, frames
+
 
 def make(fpath, width, height, output_path):
     with Image.open(fpath) as im:
         frames = load_frames(im)
 
-    colors, frames = compress(frames, width, height)
+    colors, compressed = compress(frames, width, height)
 
     with open(output_path, "w") as f:
         # Scratch treats semicolons and commas as CSV delimeters
@@ -77,8 +126,8 @@ def make(fpath, width, height, output_path):
         f.write(f"{CHARS}\n")
         f.write(f"{'-'.join(colors)}\n")  # colors
         # 3 lines for headers
-        for frame in frames:
-            f.write(f"{''.join(frame)}\n")
+        for frame in compressed:
+            f.write(f"{frame}\n")
 
 
 if __name__ == "__main__":
